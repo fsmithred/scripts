a0374f9b45572429b363f1c8025f74083bbd0ef1
fsmithred <fsmithred@gmail.com>
2010-11-07 10:06:35 -0500

    This could be a wishlist

---
+++ TODO_for_todo.sh
@@ -0,0 +1,11 @@
+TODO list for todo.sh
+
+- Display entries by date they were entered? Is this on anyone's wishlist?
+- Figure out how to do somthing like:
+    if x is in field y, do something
+- Right now, the script exits after it does almost any task. Should it keep running?
+ e.g. Maybe for deleting a line, it should loop and ask if you want to delete another.
+      (Put a while loop in the function)
+- Disable colors in my nano. Holy shit.
+
+
