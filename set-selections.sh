eca47cda17208a10cbbbf0335ba483dcb388f268
fsmithred <fsmithred@gmail.com>
2010-12-05 10:14:25 -0500

    added tests and comments

--- set-selections.sh
+++ set-selections.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 #
 # set-selections.sh
-# This script is to be used with get-selections.sh
+# Run 'dpkg --set-selections' and 'aptitude markauto'
+# using package list files created with get-selections.sh
 
+
+# You can change the directory where the lists are stored. Make sure
+# that this variable is the same in both get-selections.sh and
+# set-selections.sh. If you leave the value empty or use "$(pwd)/"
+# the list files will be in the same directory as the scripts.
 # THE DIRECTORY YOU PUT HERE NEEDS A TRAILING SLASH!
 list_dir=""
 
@@ -43,38 +49,62 @@ echo "
 To choose which files to use, enter the date/time stamp
 (example: 20101117_1289996978)
 "
-ls "$list_dir"*package*
+ls "$list_dir"package_selections_* "$list_dir"auto-packages_*
 echo "
 Enter: "
 read datetime
+
+
+# test to make sure the files you selected really exist.
+if ! [[ -f "$list_dir"package_selections_"$datetime" ]]
+then
+    echo -e "\n $list_dir"package_selections_"$datetime" not found!
+    echo -e " Exiting...\n"
+    exit 1
+fi
+if ! [[ -f "$list_dir"auto-packages_"$datetime" ]]
+then
+    echo -e "\n $list_dir"auto-packages_"$datetime" not found!
+    echo -e " Exiting...\n"
+    exit 1
+fi
+
 
 echo "
  Running apt-get update...
 "
 sleep 1
 apt-get update
+
+
 echo "
  Running dpkg --clear-selections..."
 sleep 1
 dpkg --clear-selections
+
+
+
 echo "
  Running dpkg --set-selections <" "$list_dir"package_selections_"$datetime"...
-
+echo
 sleep 1
 dpkg --set-selections < "$list_dir"package_selections_"$datetime"
+
+
 echo "
  Running apt-get -u dselect-upgrade...
  " 
 sleep 1
 apt-get -u dselect-upgrade
-echo "
- Running aptitude markauto \$(cat '$list_dir'auto-packages_$datetime)...
-"
 
+
+echo "
+ Running aptitude markauto \$(cat" "$list_dir"auto-packages_"$datetime"")..."
+echo
 sleep 1
 aptitude markauto $(cat "$list_dir"auto-packages_"$datetime")
 echo "
-And that would be it. 
+    Done!. 
 "
 
 exit 0 
